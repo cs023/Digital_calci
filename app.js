fd682e5313a9e7e08d33c9f729c9114e48d94133
Alok3 Ranjan <Alok3.Ranjan@ril.com>
2020-01-23 16:47:00 +0530

    calci

---
+++ app.js
@@ -0,0 +1,36 @@
+function display(val){
+  console.log(val)
+  document.getElementById('result').value+=val
+}
+
+function solve(){
+  let x=document.getElementById('result').value
+  // let res = x.match(/[\d\+\-\*\^\!]/g);
+  alert(x)
+  let res = x.match(/\D/g)
+  alert(res)
+  let val = x.split(res)
+  // document.getElementById('result').innerHTML=res;
+  var a;
+  if(res=="^") {
+    a = Math.pow(Number(val[0]), Number(val[1]))
+//alert(Number(res[0])+Number(res[2]))
+// alert(Math.pow(Number(res[0]), Number(res[2])))
+  } else if(res=="!"){
+    a = fact(Number(val[0]))
+  }
+  else {
+    a = eval(x)
+  }
+
+
+ document.getElementById('result').value=a
+
+}
+function clr(){
+  document.getElementById('result').value=""
+}
+function back() {
+let value = document.getElementById('result').value;
+document.getElementById('result').value = value.substr(0, value.length - 1);
+}
---
+++ fact.js
@@ -0,0 +1,10 @@
+function fact(x){
+  // alert("ppp")
+  let i, fact=1;
+  // let x=document.getElementById('result').value
+  for(i=1;i<=x;i++){
+    fact=fact*i;
+  }
+  // document.getElementById('')=fact;
+  return fact
+}
